3bd8464e24cc4dc30601da527897c759476a29f7
Sabrina Hazan <sabrinab.hazan@gmail.com>
2021-05-20 14:07:31 -0300

    millestone 4/5

--- fibonacci.js
+++ fibonacci.js
@@ -2,7 +2,7 @@
 function main()
 {
  var num = parseInt(document.getElementById("num").value);
- document.getElementById("answer").innerHTML = fibonacci(num);
+serverFibonacci(num);
 }
 
 function fibonacci(x){
@@ -10,4 +10,17 @@ function fibonacci(x){
     if(x<=2 && x>0){ return 1; }
     else if (x==0){ return 0; }
   return ( fibonacci(x-1) + fibonacci(x-2) );
+} 
+function serverFibonacci(x){
+let result= document.getElementById("answer");
+const header = {method:"GET"}
+const url="http://localhost:5050/fibonacci/"+x;
+fetch (url, header)
+  .then((response)=>{
+    if (response.ok) return response.json()
+    else return response.text()
+      })
+  .then(data =>result.innerHTML= data.result? data.result:data)
+
+
 }
